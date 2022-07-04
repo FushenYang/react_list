92f4541c66bd8aa8ee1217f1138021d495324625
yangfs <fushenyang@gmail.com>
2022-07-04 10:43:38 +0800

    例子完成。

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import { useEffect } from 'react';
+
+function App() {
+  const data = [{ id: 1, name: "学习课程" },
+  { id: 2, name: "完成作业" },
+  { id: 3, name: "阅读学习资料" },
+  { id: 4, name: "看一看胜古朝阳" },
+  ]
+
+  const [tasks, setTasks] = useState(data);
+  const [newTask, setNewTask] = useState("");
+  function handleClick(event) {
+    var NewID = tasks.map(item => item.id).reduce((acc, item) => Math.max(acc, item)) + 1;
+    // var NewID = Math.max.apply(0,tasks.map(item=>item.id)) + 1;
+    setTasks([...tasks, { id: NewID, name: newTask }]);
+  }
+  function handleDeleteTask(TaskID) {
+    setTasks(tasks.filter(e => e.id != TaskID));
+  }
+
+  useEffect(() => {
+    setNewTask("");
+  }, [tasks])
+
+  return (<>
+    <div>
+      <p>我的待办列表</p>
+      <ul>
+        {tasks.map((item) => {
+          return (
+            <li key={item.id}>
+              <span onClick={() => { handleDeleteTask(item.id) }}>[x]</span>
+              {item.name}</li>)
+        }
+        )}
+      </ul>
+    </div>
+    <div>
+      <input type="text" value={newTask} onChange={(event) => { setNewTask(event.target.value); }} />
+      <button type="button" onClick={handleClick} >添加任务</button>
+    </div>
+  </>
+  )
+}
+
+export default App
